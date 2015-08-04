283ef7e6a89275a944a775b2794dcd30736a9900
Andrew Dinh <andrewdinh74@yahoo.com>
2015-08-03 22:50:41 -0700

    Update a functional test case in the tutorial Intern framework

--- tests/functional/index.js
+++ tests/functional/index.js
@@ -1,8 +1,7 @@
-define([
-  'intern!object',
-  'intern/chai!assert',
-  'require'
-], function (registerSuite, assert, require) {
+define(function (require) {
+  var registerSuite = require('intern!object');
+  var assert = require('intern/chai!assert');
+
   registerSuite({
     name: 'index',
 
@@ -12,17 +11,17 @@ define([
         .setFindTimeout(5000)
         .findByCssSelector('body.loaded')
         .findById('nameField')
-          .click()
-          .type('Elaine')
-          .end()
+        .click()
+        .type('Elaine')
+        .end()
         .findByCssSelector('#loginForm input[type=submit]')
-          .click()
-          .end()
+        .click()
+        .end()
         .findById('greeting')
         .getVisibleText()
         .then(function (text) {
           assert.strictEqual(text, 'Hello, Elaine!',
-            'Greeting should be displayed when the form is submitted');
+          'Greeting should be displayed when the form is submitted');
         });
     }
   });
