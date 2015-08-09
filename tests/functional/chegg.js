6dba4f8f118350f4df09e20e31197430ba7a59b9
Andrew Dinh <andrewdinh74@yahoo.com>
2015-08-08 18:33:22 -0700

    Add another test case

---
+++ tests/functional/chegg.js
@@ -0,0 +1,28 @@
+define(function (require) {
+  var registerSuite = require('intern!object');
+  var assert = require('intern/chai!assert');
+
+  registerSuite({
+    name: 'index',
+
+    'greeting form': function () {
+      return this.remote
+        .get(require.toUrl('http://www.chegg.com'))
+        .setFindTimeout(7000)
+        .findByCssSelector('body.loaded')
+        .findById('#autosuggest-input')
+        .click()
+        .type('Selenium')
+        .end()
+        .findByCssSelector('a.autosuggest-search-btn')
+        .click()
+        .end()
+        .findById('greeting')
+        .getVisibleText()
+        .then(function (text) {
+          assert.strictEqual(text, 'Hello, Elaine!',
+          'Greeting should be displayed when the form is submitted');
+        });
+    }
+  });
+});
